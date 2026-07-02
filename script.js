41c0da764562ae6c2774c3e4e05697f115feb493
schmitt999 <schmitt.cristiano@escola.pr.gov.br>
2026-07-02 10:53:26 -0300

    pronto

--- script.js
+++ script.js
@@ -10,30 +10,30 @@ const caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567
 
 // Função responsável por construir a senha aleatória
 function gerarSenha() {
-    let novaSenha = '';
-    for (let i = 0; i < tamanhoSenha; i++) {
-        const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
-        novaSenha += caracteres.charAt(indiceAleatorio);
-    }
-    campoSenha.value = novaSenha;
+let novaSenha = '';
+for (let i = 0; i < tamanhoSenha; i++) {
+const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+novaSenha += caracteres.charAt(indiceAleatorio);
+}
+campoSenha.value = novaSenha;
 }
 
 // Evento que reduz o tamanho (com limite mínimo de 4 caracteres)
 btnMenos.addEventListener('click', () => {
-    if (tamanhoSenha > 4) {
-        tamanhoSenha--;
-        textoTamanho.textContent = tamanhoSenha;
-        gerarSenha();
-    }
+if (tamanhoSenha > 4) {
+tamanhoSenha--;
+textoTamanho.textContent = tamanhoSenha;
+gerarSenha();
+}
 });
 
 // Evento que aumenta o tamanho (com limite máximo de 30 caracteres)
 btnMais.addEventListener('click', () => {
-    if (tamanhoSenha < 30) {
-        tamanhoSenha++;
-        textoTamanho.textContent = tamanhoSenha;
-        gerarSenha();
-    }
+if (tamanhoSenha < 30) {
+tamanhoSenha++;
+textoTamanho.textContent = tamanhoSenha;
+gerarSenha();
+}
 });
 
 // Executa a função automaticamente ao abrir ou atualizar a página
